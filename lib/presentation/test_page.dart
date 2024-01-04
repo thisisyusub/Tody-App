5563e9b4c6d5c13257c8bbeb2c0167f5d86aebda
Kanan Yusubov <thisisyusub@Kanans-MacBook-Pro-2.local>
2024-01-04 15:13:33 +0400

    changed

--- lib/presentation/test_page.dart
+++ lib/presentation/test_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:tody_app/main.dart';
 
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    print(MediaQuery.devicePixelRatioOf(context));
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ana sehife'),
